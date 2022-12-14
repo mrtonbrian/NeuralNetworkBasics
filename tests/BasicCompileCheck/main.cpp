812976c3318dca5134e6345791ce1e4ef347fdad
Brian Ton <mrtonbrian@gmail.com>
2022-12-13 23:06:47 -0800

    Fix up test

--- tests/BasicCompileCheck/main.cpp
+++ tests/BasicCompileCheck/main.cpp
@@ -5,6 +5,9 @@
 using namespace NeuralNetworkBasics;
 
 int main() {
+    // Should compile
+    auto layer1 = std::unique_ptr<Layer<ReLU>> (new Layer<ReLU>(100, 100));
+
     // Should fail to compile since we have a bad parameter to layer
-    auto layer = std::unique_ptr<Layer<ReLU>> (new Layer<ReLU>(100, 100));
+    auto layer = std::unique_ptr<Layer<int>> (new Layer<int>(100, 100));
 }
